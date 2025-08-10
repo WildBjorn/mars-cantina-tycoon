8305f08f36f6fbeab84e030352fce1fd0e24aab4
WildBjorn <bjornjacobsen84@hotmail.com>
2025-08-10 14:30:46 -0700

    Delete src/src directory

--- src/src/scenes/src/scenes/src/scenes/src/scenes/src/scenes/src/build/src/core/src/core/Events.ts
+++
@@ -1,7 +0,0 @@
-import Phaser from 'phaser';
-
-// Central event bus for decoupling systems. Scenes can import and emit/listen
-// for events from this singleton instead of coupling directly to each other.
-const events = new Phaser.Events.EventEmitter();
-
-export default events;
